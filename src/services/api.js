460e4cc6748b347e66ef86e954193f67bc09428b
Rafael <rafaeldelima97@hotmail.com>
2020-07-15 21:32:57 -0300

    preparando deploy do front

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 // http://localhost:3001/api
-const api = axios.create({ baseURL: "https://uploadxample-backend.herokuapp.com/" })
+const api = axios.create({ baseURL: "https://uploadxample-backend.herokuapp.com" })
 
 export default api
