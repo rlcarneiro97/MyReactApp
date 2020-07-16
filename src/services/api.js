6cde0a5d8db5611c468b6baeb4a8ab4a4818558a
Rafael <rafaeldelima97@hotmail.com>
2020-07-15 21:25:19 -0300

    preparando ambiente pra deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
-const api = axios.create({ baseURL: "http://localhost:3001/api" })
+// http://localhost:3001/api
+const api = axios.create({ baseURL: "https://uploadxample-backend.herokuapp.com/" })
 
 export default api
